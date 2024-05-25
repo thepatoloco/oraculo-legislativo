9fad15297b4fcfbd0ae0b84b6fff74d3fd7e7f08
Pato Cantu <laspatocuentas@gmail.com>
2024-05-25 12:35:34 -0600

    Block chain data type

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -6,3 +6,17 @@ export type FunctionResult<T> = {
     success: false,
     error: any,
 }
+
+// Blockchain
+export type BlockInfo = {
+    file_hash: string,
+    date: Date,
+    previous_block_hash: string
+} & {
+    type: "initiative",
+    file_url: string,
+} | {
+    type: "votes",
+    content: string,
+    content_hash: string
+}
