ff4c596326c9ada5cd9d953dff52a59c964cbc64
Abigail Sosa <61513075+sadiee08@users.noreply.github.com>
2024-05-25 11:31:09 -0600

    Agrega logo y titulo

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,12 +2,16 @@ import React from 'react'
 import styles from './navbar.module.css'
 import Links from './links/Links'
 import Link from 'next/link'
+import Image from "next/image";
 
 
 const Navbar = () => {
   return (
     <div className={styles.container}>
-      <Link href="/" className={styles.logo}>Logo</Link>
+      <Link href="/" className={styles.imgContainer}>
+        <Image src="/logo.png" alt="Logo" fill/>
+      </Link>
+      
       <div>
         <Links/>
       </div>
