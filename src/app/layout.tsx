4784ffa9e91ec7b43b40795a6c02f02216134a87
Abigail Sosa <61513075+sadiee08@users.noreply.github.com>
2024-05-25 11:52:14 -0600

    Arregla logo y quita el responsive

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-      <div className="container">
+      <div className="w-full">
           <Navbar />
           {children}
           <div className="footerContainer">
