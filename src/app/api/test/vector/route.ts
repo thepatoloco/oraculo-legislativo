a6c9a44ee1997ad6c9fd860d83a3381ef9101fc4
Pato Cantu <laspatocuentas@gmail.com>
2024-05-25 13:48:01 -0600

    Testing vector

--- src/app/api/test/vector/route.ts
+++ src/app/api/test/vector/route.ts
@@ -1,3 +1,24 @@
+import { mongoConnect } from "@/utils/mongo";
+import { openai } from "@ai-sdk/openai";
+import { embed } from "ai";
+import { NextResponse } from "next/server";
+
 export async function GET() {
+    const { embedding } = await embed({
+        model: openai.embedding("text-embedding-3-small"),
+        value: "El presente Decreto entrará en vigor el día siguiente al de su publicación en el Diario Oficial de la Federación."
+    });
+    console.log(embedding);
     
+    const mongo = await mongoConnect();
+    const result = await mongo.Collection.aggregate([
+        {
+            "vectorSearch": {
+                "path": "plot_embedding",
+                "queryVector": embedding
+            }
+        }
+    ]);
+
+    return NextResponse.json({ messges: "Success!" }, { status: 200 });
 }
